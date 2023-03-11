cfb5045c8ddef9942364e8c8bd124c9485fcde23
username <username@email.com>
2023-03-11 15:19:30 +0800

    v0

---
+++ src/main/java/com/example/djjc/DjjcApplication.java
@@ -0,0 +1,13 @@
+package com.example.djjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjjcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjjcApplication.class, args);
+    }
+
+}
